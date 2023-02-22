4fd6908f7aa5795d54601ec6b55524311b2a2134
xuanyuan <xuanyuanshenfu@outlook.com>
2023-02-22 11:27:15 +0800

    add

--- Package.swift
+++ Package.swift
@@ -1,28 +1,22 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
     name: "MyLib",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MyLib",
-            targets: ["MyLib"]),
+        .library(name: "MyLib", targets: ["MyLib"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-//         .package(url: "https://github.com/shenfu1991/c2sp", from: "1.0.9"),
+       
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLib",
-            dependencies: []),
-        .testTarget(
-            name: "MyLibTests",
-            dependencies: ["MyLib"]),
+        .target(name: "MyLib", dependencies: [
+            
+        ])
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# MyLib
-
-A description of this package.
+<p align="center">
+    <img 
+        src="https://user-images.githubusercontent.com/1342803/75630258-105e9c00-5bb7-11ea-81b8-86afa000e188.png"
+        height="64" 
+        alt="NIOWebSocketClient"
+    >
+    <br>
+    <br>
+    <a href="https://docs.vapor.codes/4.0/">
+        <img src="http://img.shields.io/badge/read_the-docs-2196f3.svg" alt="API Docs">
+    </a>
+    <a href="http://vapor.team">
+        <img src="https://img.shields.io/discord/431917998102675485.svg" alt="Team Chat">
+    </a>
+    <a href="LICENSE">
+        <img src="http://img.shields.io/badge/license-MIT-brightgreen.svg" alt="MIT License">
+    </a>
+    <a href="https://github.com/vapor/websocket-kit/actions">
+        <img src="https://github.com/vapor/websocket-kit/workflows/test/badge.svg" alt="Continuous Integration">
+    </a>
+    <a href="https://swift.org">
+        <img src="http://img.shields.io/badge/swift-5.2-brightgreen.svg" alt="Swift 5.2">
+    </a>
+</p>
